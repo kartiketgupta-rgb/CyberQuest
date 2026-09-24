bb1e6793a4b29a2e489d7ce65a15c571d7d4a26f
Kartik Gupta <kartiketgupta@gmail.com>
2026-09-24 20:36:21 +0530

    pass hide

--- backend/server.js
+++ backend/server.js
@@ -22,9 +22,10 @@ const io = new Server(server, {
 app.use(express.json({ limit: "1mb" }));
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  database: "cybershield"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 let cachedQuestions = [];
